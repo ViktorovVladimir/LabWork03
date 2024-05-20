0812f1e90773f223b0a89a7ae57707495b016073
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-20 23:38:22 +0300

    Create Ex.4 Shoot on Target
    создал программу: стрельба по мишени

---
+++ Lab04/ShootOnTarget/ShootOnTarget/ShootOnTarget.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+
+namespace ShootOnTargetNameSpace
+{
+    /*
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    */
+
+    /*
+    public class WhatDay3
+    {
+        public static int[] DaysInLeapMonths = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        public static int[] DaysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    }
+    */
+
+    class ShootOnTarget
+    {
+
+        public static void Main()
+        {
+            //--.
+            try
+            {
+                //--.
+                Console.Write("Please enter the year: ");
+                int yearNum = int.Parse(Console.ReadLine());
+                bool isLeapYear = (yearNum % 4 == 0) && (yearNum % 100 != 0) || (yearNum % 400 == 0);
+                int maxDayNum = isLeapYear ? 366 : 365;
+
+                /*
+                //--.
+                if( isLeapYear ) 
+                {   
+                    Console.WriteLine("IS a leap year");
+                }   
+                else
+                {   
+                    Console.WriteLine(" is NOT a leap year");
+                }   
+                */
+                string sOtvet = isLeapYear ? "IS a leap year" : " is NOT a leap year";
+                Console.WriteLine(sOtvet);
+
+                //--.                
+                Console.WriteLine("Please enter a day number between 1 and {0}: ", maxDayNum);
+                //--.
+                int daynum = int.Parse(Console.ReadLine());
+
+                //--.
+                if (daynum < 1 || daynum > maxDayNum)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                //--.
+                int monthNum = 0;
+
+
+                if (isLeapYear)
+                {
+                    //--.
+                    foreach (int daysInMonth in WhatDay3.DaysInLeapMonths)
+                    {
+                        if (daynum <= daysInMonth)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            daynum -= daysInMonth;
+                            monthNum++;
+                        }
+                    }
+                }
+                else
+                {
+                    //--.
+                    foreach (int daysInMonth in WhatDay3.DaysInMonths)
+                    {
+                        if (daynum <= daysInMonth)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            daynum -= daysInMonth;
+                            monthNum++;
+                        }
+                    }
+                }
+
+
+
+
+
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+
+                //--.
+                Console.WriteLine("{0} {1}", daynum, monthName);
+
+
+            }
+            catch (Exception caugh)
+            {
+                Console.WriteLine("Day out of range: {0}", caugh);
+            }
+
+        }
+    }
+}
