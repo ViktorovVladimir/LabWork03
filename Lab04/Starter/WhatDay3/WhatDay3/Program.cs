f0dc95c3e250bcd5c98a6408cdbe9f7367a59c7a
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-20 21:43:47 +0300

    create ex.3
    - создал программу WhatDay3 , "Учёт високосных годов"

---
+++ Lab04/Starter/WhatDay3/WhatDay3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+namespace WhtDay3NameSpace
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    public class WhatDay3
+    {
+        public static int[] DaysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    }
+
+    class Program
+    {
+
+        public static void Main()
+        {
+            //--.
+            try
+            {
+                Console.WriteLine("Please enter a day number between 1 and 365: ");
+                //-.
+                int daynum = int.Parse(Console.ReadLine());
+                //--.
+                if (daynum < 1 || daynum > 365)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                //--.
+                int monthNum = 0;
+
+                //--.
+                foreach (int daysInMonth in WhatDay3.DaysInMonths)
+                {
+                    if (daynum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        daynum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+
+
+
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+
+                //--.
+                Console.WriteLine("{0} {1}", daynum, monthName);
+
+
+            }
+            catch (Exception caugh)
+            {
+                Console.WriteLine("Day out of range: {0}", caugh);
+            }
+
+        }
+    }
+}
